012d8ffc3cfe6214131df12529d75126bef62811
Genius Format <71953979+Genius-Format@users.noreply.github.com>
2021-09-07 16:05:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const mineflayer = require('mineflayer')
 const pvp = require('mineflayer-pvp').plugin
 const { pathfinder, Movements, goals} = require('mineflayer-pathfinder')
 const armorManager = require('mineflayer-armor-manager')
+const inventoryViewer = require('mineflayer-web-inventory')
+const { mineflayer: mineflayerViewer } = require('prismarine-viewer')
+const vec3 = require('vec3')
 const GoalFollow = goals.GoalFollow
 const GoalBlock = goals.GoalBlock
 
@@ -11,6 +14,22 @@ const bot = mineflayer.createBot({
     username: process.argv[4] || 'BOT',
     password: process.argv[5],
     logErrors: false
+})
+
+bot.once('spawn', () => {
+    console.log('Bot joined!')
+    console.log('Bot version: ' + bot.version)
+    inventoryViewer(bot)
+    mineflayerViewer(bot, { port: 3001, firstPerson: true })
+})
+
+bot.on('playerCollect', (collector, itemDrop) => {
+    if (collector !== bot.entity) return
+
+    setTimeout(() => {
+        const totem = bot.inventory.items().find(item => item.name.includes('totem of undying'))
+        if (totem) bot.equip(totem, 'off-hand')
+    }, 250)
 })
 
 bot.loadPlugin(pvp)
